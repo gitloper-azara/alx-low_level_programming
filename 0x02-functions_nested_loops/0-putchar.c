22035a6682a985c1ac43b76c7fa0e275a4971a6b
Yushahu Yussif <yyushahu02@gmail.com>
2023-06-20 13:04:09 -0700

    Printing _putchar to stdout

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - program prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0; text[i] != '\0'; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
+/**
+ * _putchar - this function prints a character
+ *
+ * @c: the character to be printed
+ *
+ * Description: prints the specified character
+ * to stdout
+ *
+ * Return: 0 (Success)
+ */
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (c);
+}
