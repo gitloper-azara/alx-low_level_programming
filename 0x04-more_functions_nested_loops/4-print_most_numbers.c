a6da863db0902c3cf537ef2281d61ac6cc4f20e5
Yushahu Yussif <yyushahu02@gmail.com>
2023-06-22 15:41:46 -0700

    !!

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ int print_most_numbers(void)
 {
 	int c;
 
-	for (c = 48; c <= 57; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == 50 && c == 52))
+		if (!(c == '2' && c == '4'))
 			_putchar(c);
 	}
 	_putchar('\n');
