bd9c9ac6ef4999c85594cf24a39a58eeaae6472e
Yushahu Yussif <yyushahu02@gmail.com>
2023-06-22 15:13:07 -0700

    print 0 to 9 minus 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * minus 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char c = 0;
+
+	if ((c >= 48 && c <= 57) !(c == 50 && c == 52))
+	{
+		_putchar(c);
+	}
+	else
+		return (0);
+}
