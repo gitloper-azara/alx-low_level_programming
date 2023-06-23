ba1360ff15018c4e0c7b919b18184e1c0dd0777c
Yushahu Yussif <yyushahu02@gmail.com>
2023-06-22 18:44:39 -0700

    debugger

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,18 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
-			printf("%i", i);
-		if (i < 100)
-			printf(" ");
+			printf("%i ", i);
 	}
 	printf("\n");
 	return (0);
