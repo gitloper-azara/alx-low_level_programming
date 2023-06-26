a9be74ff878f0401249a6e16fa570211630fcb3c
Yushahu Yussif <yyushahu02@gmail.com>
2023-06-26 15:43:01 -0700

    reverse print a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string, in reverse, followed by new_line
+ * @s: char to be checked
+ *
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+	n = 0;
+
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
