7c242217ae9e5de1bc565f86e1ab5d37fb86dcb1
Yushahu Yussif <yyushahu02@gmail.com>
2023-06-29 15:35:56 -0700

    reshuffle

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,16 +11,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (dest[i++])
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		j++;
+		continue;
 	}
-	for (i = 0; src[i] && i < n; i++)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[j++] = src[i];
+		dest[i + j] = src[j];
 	}
+	dest[i + j] = '\0';
 	return (dest);
 }
