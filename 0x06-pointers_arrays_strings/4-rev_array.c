a11bcd261b0a2a7fed70bfba4a1fca61e2072b75
Yushahu Yussif <yyushahu02@gmail.com>
2023-06-29 16:34:46 -0700

    int declaration

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,9 +13,9 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; (i < (n - 1) / 2); i++)
 	{
-		c = a[i];
+		b = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = c;
+		a[n - 1 - i] = b;
 	}
 	return (void);
 }
