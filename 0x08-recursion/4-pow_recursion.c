31266a7d384c8c5e610ae51f03f559e56fe40ce9
azara <yyushahu02@gmail.com>
2023-07-05 18:11:27 +0000

    exponentials

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power y
+ * @x: int to be raised
+ * @y: int to use to raise x
+ *
+ * Return: -1 on error
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (1);
+}
