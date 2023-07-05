ff0e8d3b8187717d91b510f6ec918e65d492ffef
azara <yyushahu02@gmail.com>
2023-07-05 00:32:46 +0000

    count the length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - gets or counts the length of a prefix substring
+ * @s: segment consisting of bytes
+ * @accept: bytes in the segment
+ *
+ * Return: number of bytes in the segment (accept)
+ */
+
+unsigned int _strspn(char *s, char *accpet)
+{
+	int z = 0;
+	int x;
+	int y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != ' ')
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+				{
+					z++;
+				}
+			}
+		}
+		else
+		{
+			return (z);
+		}
+	}
+	return (z);
+}
