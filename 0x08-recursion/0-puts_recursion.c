95f71f817f2954556d612cb82a9bc44b1bb7240b
azara <yyushahu02@gmail.com>
2023-07-05 16:54:03 +0000

    print string to stdout w recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to print
+ *
+ * Return: pointer to the string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+}
