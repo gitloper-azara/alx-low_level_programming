6a6d44d10a88c45247dcd709f978a6fb76328360
azara <yyushahu02@gmail.com>
2023-07-11 21:35:32 +0000

    40 lines

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,6 @@
  *
  * Return: NULL if unsuccessful
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
@@ -17,42 +16,25 @@ char *str_concat(char *s1, char *s2)
 	int len3, i;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
-
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	len3 = len1 + len2;
 
 	s = (char *) malloc(len3 * sizeof(char) + 1);
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		s[i] = s1[i];
-	}
-
 	for (i = 0; i < len2; i++)
-	{
 		s[i + len1] = s2[i];
-	}
-
 	return (s);
 }
