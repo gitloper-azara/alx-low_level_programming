23f3f409f832452bcfa1cd506eeebad5c8a97a3d
azara <yyushahu02@gmail.com>
2023-07-13 19:49:59 +0000

    line added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,6 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		length3 = length3 + 1;
 	else
 	{
+		length3 = length1 + n + 1;
 		length2 = n;
 	}
 	s = malloc(sizeof(char) * length3 + 1);
