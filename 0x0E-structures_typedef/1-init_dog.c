61bc58118460214fc7972da710768104e18adc4b
azara <yyushahu02@gmail.com>
2023-07-17 22:45:43 +0000

    initialize a var

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct
+ * @name: pointer to the name of dog
+ * @age: age of dog
+ * @owner: pointer to the owner of dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,2 @@
 0. Type 'struct dog'!
+1. Function that initializes a variable of type 'struct dog'
