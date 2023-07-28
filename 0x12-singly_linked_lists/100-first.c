e07c71d26ef23abc11a0454840f83c75b9087286
azara <yyushahu02@gmail.com>
2023-07-28 03:29:36 +0000

    prints before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main - print text before main function is executed
+ *
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my ");
+	printf("house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,6 @@ int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void __attribute__((constructor)) before_main(void);
 
 #endif /* LISTS_H */
