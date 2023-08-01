2ee54fed71a095f9a2dd7d8309b4b7e5a0a2f676
azara <yyushahu02@gmail.com>
2023-08-01 23:23:44 +0000

    print all

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -15,5 +18,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
 
 #endif /* LISTS_H */
