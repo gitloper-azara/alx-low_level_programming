0d232258cfba2ce2aa9f457922bc3556a38295a0
azara <yyushahu02@gmail.com>
2023-08-08 13:28:26 +0000

    correction for cp on cmd

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *string);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void exit_on_error(int code, const char *message, const char *filename);
+void exit_on_error(int code, const char *message, const char *filename, int fp);
 
 #endif /* MAIN_H */
