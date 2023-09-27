5edaae1ff0f9669ba235a64e966eaa3bfe226acc
azara <yyushahu02@gmail.com>
2023-09-27 18:49:23 +0000

    additions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: pointer to the string.
  */
 
 void _puts_recursion(char *s)
