421b902c284ed8dc163f27b09b15a03bbed97cd1
azara <yyushahu02@gmail.com>
2023-09-28 17:27:14 +0000

    palindrome wahala!

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char to check
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+		a++;
+
+	return (a);
+}
+
+/**
+ * is_palindrome_recursive - function that recursively checks the candidacy of
+ * a string to be a palindrome.
+ * @s: pointer to the string.
+ * @start: first character to check.
+ * @end: last character to be compared to.
+ *
+ * Return: 1 on success, 0 if not a palindrome.
+ */
+
+int is_palindrome_recursive(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	else if (s[start] == s[end])
+		return (is_palindrome_recursive(s, start + 1, end - 1));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - function that checks if a string is a palindrome using
+ * recursion.
+ * @s: pointer to the string.
+ *
+ * Return: 1 on success, 0 if not a palindrome.
+ */
+
+int is_palindrome(char *s)
+{
+	int length = _strlen(s);
+
+	return (is_palindrome_recursive(s, 0, length - 1));
+}
