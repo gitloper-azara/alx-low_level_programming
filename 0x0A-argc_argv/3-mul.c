3962e74d65b63e422ac6e2a68659d0021a6badb7
azara <yyushahu02@gmail.com>
2023-09-29 20:14:47 +0000

    multiply argv on cmd

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @agrc: num of arguments
+ * @argv: argument array
+ *
+ * Return: 0 on success, error and -1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y, z;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		z = (x * y);
+		printf("%d\n", z);
+	}
+	return (0);
+}
