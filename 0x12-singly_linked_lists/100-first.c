fea03a54c1c39468660e8818e04668b4ba6a07c5
azara <yyushahu02@gmail.com>
2023-10-20 02:47:48 +0000

    before main executes!

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * before_main - a function that prints text before the main function is
+ * executed.
+ *
+ * Return: void.
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my ");
+	printf("house upon my back!\n");
+}
