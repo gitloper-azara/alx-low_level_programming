709255be2aa77888cb2c7df0ad558d2267121581
azara <yyushahu02@gmail.com>
2023-10-24 00:19:19 +0000

    check null dest

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (newNode);
 	}
 	currentNode = *head;
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; currentNode != NULL && i < idx - 1; i++)
 		currentNode = currentNode->next;
 	if (currentNode == NULL)
 	{
