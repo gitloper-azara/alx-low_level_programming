9ac2b70e7a359d730cc3acc9ed94d8a2f33fbf76
azara <yyushahu02@gmail.com>
2023-10-26 17:03:37 +0000

    comment

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
+	/* check if b is NULL */
 	if (b == NULL)
 		return (0);
 
 	while (*b)
 	{
+		/* check for the absence of 0 and 1 */
 		if (*b != '0' && *b != '1')
 			return (0);
+		/* convert 0 and 1 to uint */
 		result = result * 2 + (*b - '0');
 		b++;
 	}
