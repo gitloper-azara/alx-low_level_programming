19f6c96c86557a3ce12d0dbe600ea8427dbfbcab
azara <yyushahu02@gmail.com>
2023-10-27 00:22:01 +0000

    flip bits!

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - a function that returns the number of bits needed to flip from
+ * one number to another
+ * @n: first num
+ * @m: second num
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int numDigits = 0;
+
+	while (xor > 0)
+	{
+		numDigits += (xor & 1);
+		xor = xor >> 1;
+	}
+	return (numDigits);
+}
