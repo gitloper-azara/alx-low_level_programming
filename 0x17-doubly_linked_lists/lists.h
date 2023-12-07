009540b9a1f9372735ca0c61cbfa04dca404db76
azara <yyushahu02@gmail.com>
2023-12-07 14:54:36 +0000

    header betty

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -9,16 +9,16 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- * 
+ *
  * Description: doubly linked list node structure
- * 
+ *
 */
 
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *next;
-    struct dlistint_s *prev;
+	int n;
+	struct dlistint_s *next;
+	struct dlistint_s *prev;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
