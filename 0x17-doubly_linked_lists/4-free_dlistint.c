1e6e7894cc3726b873630810fcb384603280dcd0
azara <yyushahu02@gmail.com>
2023-12-07 19:48:17 +0000

    try this

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,15 +8,15 @@
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
+	dlistint_t *temp;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return;
 
-    while (head)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
